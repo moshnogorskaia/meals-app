9df44afd451331e2de97dd877e0fb6c08cf5e64b
Marina Moshnogorskaia <marina.moshnogorskaia@erasys.de>
2025-03-14 16:20:13 +0100

    Extract the header button

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -11,6 +11,7 @@ import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import { useLayoutEffect } from "react";
+import IconButton from "../components/IconButton";
 
 function MealDetailScreen({ route, navigation }) {
 	const { mealId } = route.params;
@@ -24,7 +25,13 @@ function MealDetailScreen({ route, navigation }) {
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			headerRight: () => {
-				return <Button title="Tap me" onPress={headerButtonPressHandler} />;
+				return (
+					<IconButton
+						icon="star"
+						color="white"
+						onPress={headerButtonPressHandler}
+					/>
+				);
 			},
 		});
 		// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
