71f1dba676cdfc305f534504ea159d3afdf867b0
Marina Moshnogorskaia <marina.moshnogorskaia@erasys.de>
2025-03-17 13:04:24 +0100

    Add nested Drawer Navigation

--- App.js
+++ App.js
@@ -5,8 +5,32 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+	return (
+		<Drawer.Navigator
+			screenOptions={{
+				headerStyle: { backgroundColor: "#903013" },
+				headerTintColor: "white",
+				sceneContainerStyle: { backgroundColor: "#662e1d" },
+			}}
+		>
+			<Drawer.Screen
+				name="Categories"
+				component={CategoriesScreen}
+				options={{
+					title: "All Categories",
+				}}
+			/>
+			<Drawer.Screen name="Favorites" component={FavoritesScreen} />
+		</Drawer.Navigator>
+	);
+}
 
 export default function App() {
 	return (
@@ -21,14 +45,20 @@ export default function App() {
 					}}
 				>
 					<Stack.Screen
-						name="MealsCategories"
-						component={CategoriesScreen}
+						name="Drawer"
+						component={DrawerNavigator}
 						options={{
-							title: "All Categories",
+							headerShown: false,
 						}}
 					/>
 					<Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-					<Stack.Screen name="MealDetail" component={MealDetailScreen} />
+					<Stack.Screen
+						name="MealDetail"
+						component={MealDetailScreen}
+						options={{
+							title: "About the Meal",
+						}}
+					/>
 				</Stack.Navigator>
 			</NavigationContainer>
 		</>
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function FavoritesScreen() {
+	return (
+		<View style={styles.container}>
+			<Text>Favorites Screen</Text>
+		</View>
+	);
+}
+
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+	container: {},
+});
